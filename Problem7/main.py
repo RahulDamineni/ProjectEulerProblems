3f49278c0f3796d36d8fc0a00f6681db10e40e77
RahulDamineni <rahul.damineni@cloudsek.com>
2017-09-01 18:13:38 +0530

    Final solution, but slightly lamer. Let's see how the best one works.

--- Problem7/main.py
+++ Problem7/main.py
@@ -1,4 +1,3 @@
-#!/bin/python
 
 import sys
 
@@ -28,8 +27,22 @@ def is_prime(n):
     return True
 
 
+def is_prime_v2(n, known_primes):
+    """ I thought, the basis(composition) of every odd number must
+        be a prime number. So if a prime can't divide it, no other odd can.
+        Also, if we include 2 to list, no other number can.
+        That list is precisely `known_primes`"""
+
+    for prime in known_primes.values():
+
+        if n % prime == 0:
+            return False
+
+    return True
+
+
 def range_generator(start, stop, step=1):
-    """ Just realized range() isn't a generator. Quickly writing my own generator """
+    """ Just realized range() isn't a generator. Writing my own generator """
     val = start - step
     while val < stop:
         val += step
@@ -40,16 +53,23 @@ if __name__ == '__main__':
     primes = {1: 2, 2: 3}
     max_n = 2
     t = int(raw_input().strip())
-    for a0 in xrange(t):
+    # The approach got a little lamer here, so it passes test#1
+    # To save unnecessary comparaisions, I am calculating the master list once,
+    # and printing all given inputs (max 10^3) by reference.
+    n = 10001
+    if n < max_n:
+        # print primes[n]
+        pass
+    else:
+        latest = primes[max_n]
+        for test_num in range_generator(latest + 2, 10**20, 2):
+            if is_prime_v2(test_num, primes):
+                primes.update({max_n + 1: test_num})
+                max_n += 1
+            if max_n == n:
+                # print primes[max_n]
+                break
+
+    for _ in range(t):
         n = int(raw_input().strip())
-        if n < max_n:
-            print primes[n]
-        else:
-            latest = primes[max_n]
-            for test_num in range_generator(latest + 2, 10**20, 2):
-                if is_prime(test_num):
-                    primes.update({max_n + 1: test_num})
-                    max_n += 1
-                if max_n == n:
-                    print primes[max_n]
-                    break
+        print primes[n]
